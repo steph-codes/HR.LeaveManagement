d723ad42902d7790bcefa053873a27bf8d9d873b
steph-codes <ogundele370@gmail.com>
2023-01-20 17:33:38 +0100

    Added EF Core and also Folder Structure for Infrastructure and persistence

---
+++ HR.LeaveManagement.Persistence/LeaveManagementDbContext.cs
@@ -0,0 +1,46 @@
+﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public class LeaveManagementDbContext : DbContext
+    {
+        public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options) : base(options)
+        {
+            
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                //when modified set modified date
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+
+                //when added set added date
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+
+            //calls the SaveChanges method i the bckground / its an automated process by EFCore
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+        }
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocaations { get; set; }
+    }
+}
