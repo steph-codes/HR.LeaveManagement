6eacebfc3e284a5a4ab902d7df235e84863f4157
steph-codes <ogundele370@gmail.com>
2023-01-21 09:56:23 +0100

    EmailService bug Fixed, Used  "Microsoft.Extensions.Options" Version="6.0.0" /> instead of "Microsoft.Extensions.Options.ConfigurationExtensions" Version="5.0.0" />

--- HR.LeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
+++ HR.LeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -11,10 +11,12 @@ namespace HR.LeaveManagement.Infrastructure
 {
     public static class InfrastructureServiceRegistration
     {
+        //N.B <PackageReference Include="Microsoft.Extensions.Configuration" Version="6.0.0" /> and this <PackageReference Include = "Microsoft.Extensions.Options" Version="6.0.0" />
+        //might throw error advice you use this  <PackageReference Include="Microsoft.Extensions.Options.ConfigurationExtensions" Version="5.0.0" />
         public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
-            //services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
-            //services.AddTransient<IEmailSender, EmailSender>();
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
 
             return services;
         }
